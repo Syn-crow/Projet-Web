636a60dd1908ce2d6ce567e889ff0f5e5252b157
Syn-crow <valehue@hotmail.fr>
2018-11-19 23:38:49 +0100

    fin de soirée, amélioration de l'interface!

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 divStart = document.getElementById("start");
 divJeu = document.getElementById("jeu");
 divMap = document.getElementById("map");
-divDialogue = document.getElementById("dialogue")
+divDialogue = document.getElementById("dialogue");
+divOverlay = document.getElementById("overlay");
+btOverlayO = document.getElementById("overlayOpen");
+btOverlayC = document.getElementById("overlayClose");
 
-btStart.addEventListener("click",start);
+message = document.getElementById("message");
+
 
 
 //initialisation
@@ -24,6 +28,38 @@ function start(){
   }).addTo(map); // on initialise la map seulement
   //la car on utilise pas la carte avant
 }
+//passe de map à dialogue et vice-versa
+function mapDialog(){
+  if(divJeu.style.display == "none"){
+    divJeu.style.display = "block";
+    divDialogue.style.display = "none";
+  }else{
+    divJeu.style.display = "none";
+    divDialogue.style.display = "block";
+  }
+}
+
+function hideShow(){
+  if(divOverlay.style.display == "none"){
+    divOverlay.style.display = "block";
+    btOverlayC.style.display = "block";
+    btOverlayO.style.display = "none";
+  }
+  else{
+    divOverlay.style.display = "none"
+    btOverlayC.style.display = "none";
+    btOverlayO.style.display = "block";
+  }
+}
+function setMessage(text){
+  message.innerHTML = text;
+}
+
+setMessage("bienvenue, ceci est un message écrit en utilisant la fonction setMessage!")
 
 //ajout des events
 btStart = document.getElementById("btStart");
+btStart.addEventListener("click",start);
+
+btOverlayC.addEventListener("click",hideShow);
+btOverlayO.addEventListener("click",hideShow);
