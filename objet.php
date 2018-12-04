2fd4c71084f8a9a273f3609eaca9ff63159baea1
Tonksev <pyanzhuo27@yahoo.fr>
2018-12-05 00:58:30 +0100

    changement des noms des paramètres dans les requêtes

--- objet.php
+++ objet.php
@@ -10,7 +10,7 @@ if (!$link) {
 
 
 if ($_POST['request'] == "code1") {
-    $requete = "SELECT id,nom,image,latitude,longitude,type_condition,parametre,message,indice,image_pnj FROM objets WHERE id=1";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=1";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -20,7 +20,7 @@ if ($_POST['request'] == "code1") {
 }
 
 if ($_POST['request'] == "pendentif") {
-    $requete = "SELECT id,nom,image,latitude,longitude,type_condition,parametre,message,indice,image_pnj FROM objets WHERE id=2";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=2";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -30,7 +30,7 @@ if ($_POST['request'] == "pendentif") {
 }
 
 if ($_POST['request'] == "panneau") {
-    $requete = "SELECT id,nom,image,latitude,longitude,type_condition,parametre,message,indice,image_pnj FROM objets WHERE id=3";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=3";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -40,7 +40,7 @@ if ($_POST['request'] == "panneau") {
 }
 
 if ($_POST['request'] == "code2") {
-    $requete = "SELECT id,nom,image,latitude,longitude,type_condition,parametre,message,indice,image_pnj FROM objets WHERE id=4";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=4";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -50,7 +50,7 @@ if ($_POST['request'] == "code2") {
 }
 
 if ($_POST['request'] == "porte1") {
-    $requete = "SELECT id,nom,image,latitude,longitude,type_condition,parametre,message,indice,image_pnj FROM objets WHERE id=5";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=5";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -60,7 +60,7 @@ if ($_POST['request'] == "porte1") {
 }
 
 if ($_POST['request'] == "cle") {
-    $requete = "SELECT id,nom,image,latitude,longitude,type_condition,parametre,message,indice,image_pnj FROM objets WHERE id=6";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=6";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -70,7 +70,7 @@ if ($_POST['request'] == "cle") {
 }
 
 if ($_POST['request'] == "porte2") {
-    $requete = "SELECT id,nom,image,latitude,longitude,type_condition,parametre,message,indice,image_pnj FROM objets WHERE id=7";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=7";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -80,7 +80,7 @@ if ($_POST['request'] == "porte2") {
 }
 
 if ($_POST['request'] == "cadenas1") {
-    $requete = "SELECT id,nom,image,position,type_condition,parametre,message,indice FROM objets WHERE id=8";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=8";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
@@ -90,7 +90,7 @@ if ($_POST['request'] == "cadenas1") {
 }
 
 if ($_POST['request'] == "cadenas2") {
-    $requete = "SELECT id,nom,image,position,type_condition,parametre,message,indice FROM objets WHERE id=9";
+    $requete = "SELECT id,Nom,img,latitude,longitude,typeCond,parametre,message,indice,taille,zoom,ancreX,ancreY,imagePNJ FROM objets WHERE id=9";
     if ($result = mysqli_query($link,$requete)) {
         while ($ligne = mysqli_fetch_assoc($result)) {
             $tab[] = $ligne;
